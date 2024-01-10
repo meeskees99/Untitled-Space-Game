41173f02256bd0b46d27e3eed50ac6c7b599026b
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2024-01-10 16:54:45 +0100

    Start "quest" System

--- Untitled-Space-Game/Assets/Scripts/Quest/Quest.cs
+++ Untitled-Space-Game/Assets/Scripts/Quest/Quest.cs
@@ -7,6 +7,16 @@ public class Quest : ScriptableObject
 {
     public string questName;
     public bool isQuestLine;
+    public string questLineName;
     public Quest nextQuest;
     public ItemInfo[] itemsNeeded;
+
+    public enum QuestType
+    {
+        INVENTORY,
+        REPAIR,
+        PLACE,
+    }
+
+    public QuestType questType;
 }
--- Untitled-Space-Game/Assets/Scripts/Quest/QuestManager.cs
+++ Untitled-Space-Game/Assets/Scripts/Quest/QuestManager.cs
@@ -4,5 +4,70 @@ using UnityEngine;
 
 public class QuestManager : MonoBehaviour
 {
+    [SerializeField] Quest[] _quests;
+    [SerializeField] int _questsIndex;
+    [SerializeField] Quest _currentQuest;
+    [SerializeField] string _currentQuestName;
+    [SerializeField] ItemInfo[] _questRequirements;
+    public void StartNewQuest()
+    {
+        _questsIndex++;
+        _currentQuest = _quests[_questsIndex];
+        _currentQuestName = _currentQuest.questName;
+        _questRequirements = _currentQuest.itemsNeeded;
+    }
 
+    private void Update()
+    {
+        switch (_currentQuest.questType)
+        {
+            case Quest.QuestType.PLACE:
+                {
+                    CheckPlace();
+                    break;
+                }
+            case Quest.QuestType.INVENTORY:
+                {
+                    CheckInventory();
+                    break;
+                }
+            case Quest.QuestType.REPAIR:
+                {
+                    CheckRepair();
+                    break;
+                }
+        }
+    }
+
+    public void CheckPlace()
+    {
+
+    }
+
+    public bool CheckInventory()
+    {
+        for (int i = 0; i < _questRequirements.Length; i++)
+        {
+            for (int j = 0; j < InventoryManager.Instance.itemsInInventory.Count; j++)
+            {
+                if (_questRequirements[i].item == InventoryManager.Instance.itemsInInventory[j].item)
+                {
+                    if (_questRequirements[i].amount <= InventoryManager.Instance.itemsInInventory[j].amount)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public void CheckRepair()
+    {
+
+    }
 }
